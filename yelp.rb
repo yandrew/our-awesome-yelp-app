96e4cd38682091a2c9b568423b998294a60f231c
jessiewuwu <jwu004@gmail.com>
2015-01-30 14:08:02 -0800

    added two options: search and view, restaurant display method and show bookmarked method

--- yelp.rb
+++ yelp.rb
@@ -4,16 +4,36 @@ require_relative 'config/application'
 
 
 # Controller.options(ARGV[0], ARGV[1..-1])
-# options = ['search', 'display  ]
+def show_bookmarks
+  Restaurant.all.each_with_index do |restaurant, index|
+        restaurant_display(restaurant, index)
+      end
+end
+
+def restaurant_display(restaurant, index)
+  puts "*~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~*"
+        puts "#{ index + 1 }: #{restaurant.name}"
+        puts "Distance: #{restaurant.distance} meters"
+        puts "Rating: #{restaurant.rating}"
+end
 
 
 puts "*~*~*~*~* WELCOME TO YELPIE *~*~*~*~*"
 sleep(0.5)
 puts "...for broke ass DBC students"
 sleep(1.5)
-puts "What type of food do you want to devour"
-food_type = gets.chomp
-puts
+puts "What would you like to do: search or view bookmarks"
+@user_choice = gets.chomp
+case @user_choice
+when "search"
+  puts "What type of food do you want to devour?"
+  food_type = gets.chomp
+when "view"
+  show_bookmarks
+end
+
+
+
 
 
 
@@ -36,3 +56,6 @@ end
 #puts response.businesses[0].rating
 
 # puts response.businesses[0].display_address
+#name, distance, rating
+#
+
