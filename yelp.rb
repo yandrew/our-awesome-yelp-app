cf3390f5ae49ef177762b8daf6dd35f59d32bf31
Ukeeyo <yukior@gmail.com>
2015-01-29 21:00:47 -0800

    Added stuff

--- yelp.rb
+++ yelp.rb
@@ -40,9 +40,9 @@ coordinates = { latitude: 37.784900, longitude: -122.397398}
 response = Yelp.client.search_by_coordinates(coordinates, parameters)
 puts "*~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~*"
 response.businesses.each do |x|
-  ap x.name
-  ap x.distance.floor
-  ap x.rating
+  ap "                     #{x.name}"
+  ap "Distance: #{x.distance.floor}"
+  ap "Rating: #{x.rating}"
   puts "*~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~*"
 end
 #puts response.businesses[0].rating
