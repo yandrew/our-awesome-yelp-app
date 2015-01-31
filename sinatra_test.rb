fae09a7fe1d5d6fec461ebfc360aaac1cc5c6cc6
Yukio Rattai <yukior@gmail.com>
2015-01-30 16:58:33 -0800

    SINATRAgit add .git add .

--- sinatra_test.rb
+++ sinatra_test.rb
@@ -1,17 +1,61 @@
-# require_relative 'config/application'
+require_relative 'config/application'
 require 'sinatra'
 require 'shotgun'
 
+#####################################################################
+# get '/' do
+#   erb :home
+# end
+
+# post '/' do
+#   erb :search
+# end
+
+# post '/eval' do
+#   "I got #{params[:command]}."
+# end
+################################################################
+
+
 get '/' do
   erb :home
+ # erb "<form method=POST><input name=mess></form>"
 end
 
-post '/' do
+post "/" do
+  coordinates = { latitude: 37.784900, longitude: -122.397398 }
+  @search = Yelp.client.search_by_coordinates(coordinates, term: params[:type]).businesses
+ # erb "<%= escape_html(@search.each { |x| x.name }) %>"
+ if params[:type] != nil
+  erb :search
+else
+  Restaurant.create( name: params[:bookmark_choice] )
+  erb :bookmark
+end
 
-rest = Restaurant.new
-rest.address = params[:location]
-rest.name = 'Chaat'
-rest.save
+end
+
+# post '/' do
+#   erb :search
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# rest = Restaurant.new
+# rest.address = params[:location]
+# rest.name = 'Chaat'
+# rest.save
 #  parameters = { term: params[:type],
 #   limit: 10,
 #   radius_filter: 1600, #in meters = 1 miles
@@ -29,11 +73,10 @@ rest.save
   # n.updated_at = Time.now
   # n.save
   # redirect '/'
-end
 
-not_found do
-  halt 404, 'woaaahhh, wooahh, woah..... hold up..'
-end
+# not_found do
+#   halt 404, 'woaaahhh, wooahh, woah..... hold up..'
+# end
 
 # get '/hello/:name' do
 #   params[:name]
--- yelp.rb
+++ yelp.rb
@@ -25,9 +25,9 @@ parameters = { term: food_type,
 
 coordinates = { latitude: 37.784900, longitude: -122.397398 }
 
-response = Yelp.client.search_by_coordinates(coordinates, parameters)
+@response = Yelp.client.search_by_coordinates(coordinates, parameters)
 puts "*~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~**~*~*~*~*"
-response.businesses.each do |x|
+@response.businesses.each do |x|
   puts "                     #{x.name} "
   ap "Distance: #{x.distance.floor}"
   ap "Rating: #{x.rating}"
@@ -36,3 +36,89 @@ end
 #puts response.businesses[0].rating
 
 # puts response.businesses[0].display_address
+
+
+
+
+require_relative '../../config/application'
+
+
+def show_bookmarks
+  Restaurant.all.each_with_index do |restaurant, index|
+    restaurant_display(restaurant, index)
+  end
+end
+
+def restaurant_display(restaurant, index)
+  puts "\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+  puts "#{ index + 1 }: #{restaurant.name}".white_on_blue
+  ap "Distance: #{restaurant.distance} meters"
+  ap "Rating: #{restaurant.rating}"
+end
+
+def show_results(result)
+  puts "\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+  result.businesses.each_with_index do |x, index|
+    ap "#{index}                     #{x.name}"
+    # ap "#{response.location.display_address}"
+    ap "Distance: #{x.distance.floor}"
+    ap "Rating: #{x.rating}"
+    puts "\n::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::"
+  end
+  bookmark(result)
+end
+
+def bookmark(response)
+  puts "Would you like to bookmark any of these restaurants? (yes or no)".cyan
+  first_choice = gets.chomp.downcase
+  if first_choice == "yes"
+    puts "What number do you want to bookmark?".cyan
+    bookmark_choice = gets.chomp
+    restaurant_obj = response.businesses[bookmark_choice.to_i]
+    new_restaurant = Restaurant.create( name: restaurant_obj.name, distance: restaurant_obj.distance.floor, rating: restaurant_obj.rating )
+    puts "#{new_restaurant.name} has been bookmarked.".cyan
+  else
+    return "Thanks for using Yelpie".cyan
+  end
+
+end
+
+@coordinates = { latitude: 37.784900, longitude: -122.397398 }
+
+def search(term)
+  parameters = { term: term,
+    limit: 10,
+  radius_filter: 1600, #in meters = 1 miles
+  sort: 1, # 0:best matched(default), 1: distance, 2: highest rated
+}
+@response = Yelp.client.search_by_coordinates(@coordinates, parameters)
+
+end
+
+def start
+  puts "                                     ".white_on_cyan
+  puts "*~*~*~*~* WELCOME TO YELPIE *~*~*~*~*".white_on_cyan.bold
+  puts "                                     ".white_on_cyan
+  sleep(0.5)
+  puts "    lazy and hungry DBC students    ".bold.cyan
+  puts ""
+  loop do
+    puts "What would you like to do: ".cyan + "search".underline.red + ", " + "view".underline.red + " bookmarks" + ", or " + "exit".underline.red + "?".cyan
+    @user_choice = gets.chomp
+    case @user_choice.downcase
+    when "search"
+      puts "\nWhat type of food do you want to devour?".cyan
+      food_type = gets.chomp
+      search(food_type)
+      show_results(@response) if @response.businesses != []
+      puts ""
+
+    when "view"
+      show_bookmarks
+
+    when "exit"
+      abort("Thanks for using our awesome app.".red)
+
+    end
+  end
+end
